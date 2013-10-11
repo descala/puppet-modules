f1871d5e24cf2775ee2019fd1a0988b7106c16ce
Lluis Gili <lluis@ingent.net>
2013-10-11 14:31:19 +0200

    require apt conf before installing libreoffice

--- libreoffice/manifests/calc.pp
+++ libreoffice/manifests/calc.pp
@@ -1,5 +1,6 @@
 class libreoffice::calc inherits libreoffice {
-  package { "libreoffice-calc":
-    ensure => installed
+  package { 'libreoffice-calc':
+    ensure  => installed,
+    require => File['/etc/apt/preferences.d/11_libreoffice.pref']
   }
 }
--- libreoffice/manifests/writer.pp
+++ libreoffice/manifests/writer.pp
@@ -1,5 +1,6 @@
 class libreoffice::writer inherits libreoffice {
-  package { "libreoffice-writer":
-    ensure => installed
+  package { 'libreoffice-writer':
+    ensure  => installed,
+    require => File['/etc/apt/preferences.d/11_libreoffice.pref']
   }
 }

43a3be2e2c556e83055e58ddbf4dce9177d4c1e4
Lluis Gili <lluis@ingent.net>
2013-10-11 14:25:08 +0200

    fix sources.list location mistake

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -13,23 +13,23 @@ class apt inherits apt::params {
     ensure  => present,
     content => template('apt/jessie.sources.list.erb')
   }
-  file { '/etc/apt/backports.sources.list':
+  file { '/etc/apt/sources.list.d/backports.sources.list':
     ensure  => present,
     content => template('apt/backports.sources.list.erb')
   }
-  file { '/etc/apt/quantum.sources.list':
+  file { '/etc/apt/sources.list.d/quantum.sources.list':
     ensure  => present,
     content => template('apt/quantum.sources.list.erb')
   }
-  file { '/etc/apt/postgis.sources.list':
+  file { '/etc/apt/sources.list.d/postgis.sources.list':
     ensure  => present,
     content => template('apt/postgis.sources.list.erb')
   }
-  file { '/etc/apt/owncloud.sources.list':
+  file { '/etc/apt/sources.list.d/owncloud.sources.list':
     ensure  => present,
     content => template('apt/owncloud.sources.list.erb')
   }
-  file { '/etc/apt/virtualbox.sources.list':
+  file { '/etc/apt/sources.list.d/virtualbox.sources.list':
     ensure  => present,
     content => template('apt/virtualbox.sources.list.erb')
   }

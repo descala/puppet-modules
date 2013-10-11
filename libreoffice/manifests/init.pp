cc1cefff7ec030d80894cf0f7e00142703b8fc02
Lluis Gili <lluis@ingent.net>
2013-10-11 14:27:50 +0200

    add /etc/apt/preferences.d/11_libreoffice.pref

--- libreoffice/manifests/init.pp
+++ libreoffice/manifests/init.pp
@@ -1,2 +1,10 @@
 class libreoffice inherits libreoffice::params {
+
+  # libreoffice from wheezy-backports
+  file { '/etc/apt/preferences.d/11_libreoffice.pref':
+    ensure  => present,
+    content => template('libreoffice/11_libreoffice.pref'),
+    notify  => Exec['apt-update']
+  }
+
 }

6e57d04ce403031d5837ab7e37b25a8fd0ec33a0
Lluis Gili <lluis@ingent.net>
2013-10-11 14:05:03 +0200

    add needed apt sources and preferences

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -9,6 +9,30 @@ class apt inherits apt::params {
     ensure  => present,
     content => template("apt/sources.list.erb")
   }
+  file { "/etc/apt/sources.list.d/jessie":
+    ensure  => present,
+    content => template("apt/jessie.sources.list.erb")
+  }
+  file { "/etc/apt/backports.sources.list":
+    ensure  => present,
+    content => template("apt/backports.sources.list.erb")
+  }
+  file { "/etc/apt/quantum.sources.list":
+    ensure  => present,
+    content => template("apt/quantum.sources.list.erb")
+  }
+  file { "/etc/apt/postgis.sources.list":
+    ensure  => present,
+    content => template("apt/postgis.sources.list.erb")
+  }
+  file { "/etc/apt/owncloud.sources.list":
+    ensure  => present,
+    content => template("apt/owncloud.sources.list.erb")
+  }
+  file { "/etc/apt/virtualbox.sources.list":
+    ensure  => present,
+    content => template("apt/virtualbox.sources.list.erb")
+  }
   exec { "apt-update":
     refreshonly => true,
     command     => "/usr/bin/apt-get update"

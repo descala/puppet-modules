f9fbffe4556e52537ca37bb8d4676c3254ffb608
Lluis Gili <lluis@ingent.net>
2013-10-11 14:10:33 +0200

    fix jessie sources.list

--- apt/manifests/init.pp
+++ apt/manifests/init.pp
@@ -1,40 +1,40 @@
 class apt inherits apt::params {
-  package { "apt": 
+  package { 'apt':
     ensure  => installed
   }
-  package { "debian-keyring": 
+  package { 'debian-keyring':
     ensure => installed
   }
-  file { "/etc/apt/sources.list":
+  file { '/etc/apt/sources.list':
     ensure  => present,
-    content => template("apt/sources.list.erb")
+    content => template('apt/sources.list.erb')
   }
-  file { "/etc/apt/sources.list.d/jessie":
+  file { '/etc/apt/sources.list.d/jessie.sources.list':
     ensure  => present,
-    content => template("apt/jessie.sources.list.erb")
+    content => template('apt/jessie.sources.list.erb')
   }
-  file { "/etc/apt/backports.sources.list":
+  file { '/etc/apt/backports.sources.list':
     ensure  => present,
-    content => template("apt/backports.sources.list.erb")
+    content => template('apt/backports.sources.list.erb')
   }
-  file { "/etc/apt/quantum.sources.list":
+  file { '/etc/apt/quantum.sources.list':
     ensure  => present,
-    content => template("apt/quantum.sources.list.erb")
+    content => template('apt/quantum.sources.list.erb')
   }
-  file { "/etc/apt/postgis.sources.list":
+  file { '/etc/apt/postgis.sources.list':
     ensure  => present,
-    content => template("apt/postgis.sources.list.erb")
+    content => template('apt/postgis.sources.list.erb')
   }
-  file { "/etc/apt/owncloud.sources.list":
+  file { '/etc/apt/owncloud.sources.list':
     ensure  => present,
-    content => template("apt/owncloud.sources.list.erb")
+    content => template('apt/owncloud.sources.list.erb')
   }
-  file { "/etc/apt/virtualbox.sources.list":
+  file { '/etc/apt/virtualbox.sources.list':
     ensure  => present,
-    content => template("apt/virtualbox.sources.list.erb")
+    content => template('apt/virtualbox.sources.list.erb')
   }
-  exec { "apt-update":
+  exec { 'apt-update':
     refreshonly => true,
-    command     => "/usr/bin/apt-get update"
+    command     => '/usr/bin/apt-get update'
   }
 }

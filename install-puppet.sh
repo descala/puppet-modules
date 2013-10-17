1fe6d26b5aec9a5d9e734e5178c567d6a529135a
David Escala <descala@ingent.net>
2013-10-17 17:06:14 +0200

    install puppet from puppetlabs apt

--- install-puppet.sh
+++ install-puppet.sh
@@ -14,24 +14,22 @@ deb-src http://http.debian.net/debian/ jessie main contrib non-free
 EOF
 fi
 
+if [ ! -e /etc/apt/sources.list.d/puppetlabs.list ]; then
+  cat << EOF > /etc/apt/sources.list.d/puppetlabs.list
+deb http://apt.puppetlabs.com wheezy main dependencies
+EOF
+fi
+
 if [ ! -e /etc/apt/preferences.d/1_stable.pref ]; then
   cat << EOF > /etc/apt/preferences.d/1_stable.pref 
 Package: *
-Pin: release a=stable
+Pin: release n=wheezy
 Pin-Priority: 900
 EOF
 fi
 
-if [ ! -e /etc/apt/preferences.d/10_puppet.pref ]; then
-  cat << EOF > /etc/apt/preferences.d/10_puppet.pref 
-Package: puppet*
-Pin: release a=jessie
-Pin-Priority: 990
-EOF
-fi
-
 apt-get update
-apt-get install puppet -t jessie
+apt-get install puppet
 
 # disable puppet runs
 # we will always "puppet apply"
